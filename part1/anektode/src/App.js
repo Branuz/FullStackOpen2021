9fa2bbb15ece95a88cd908a1190db800d75a4243
Jyri <jyri.poussu@gmail.com>
2021-03-20 16:03:21 +0200

    1.12-1.14

--- part1/anektode/src/App.js
+++ part1/anektode/src/App.js
@@ -1,6 +1,12 @@
 import React, { useState } from 'react'
 
+let randomNumber = 0
+
 const App = () => {
+  const numberGenerator = (props) =>{
+    randomNumber = Math.floor(Math.random() * 6)
+    setSelected(randomNumber)
+}
   const anecdotes = [
     'If it hurts, do it more often',
     'Adding manpower to a late software project makes it later!',
@@ -9,12 +15,46 @@ const App = () => {
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
   ]
-   
-  const [selected, setSelected] = useState(0)
 
+  const [selected, setSelected] = useState(0)
+  const [votes, insertVotes] = useState(new Array(anecdotes.length).fill(0));
+
+
+  const handelVote = () =>{
+    const copyVotes = [...votes];
+    copyVotes[selected] += 1
+    insertVotes(copyVotes) 
+    console.log(votes)
+  }
+
+  const checkMax = () =>{
+    
+    return Math.max(...votes)
+  }
+
+  const mostVoted = () =>{
+    var i=0
+    for(i = 0; i < votes.length; i++){
+      if(votes[i]===checkMax()){
+        break
+      }
+    }
+    return (i)
+    }
+    
+  
   return (
     <div>
+      <h1>Anectode of the day</h1>
       {anecdotes[selected]}
+      <p>Has {votes[randomNumber]} votes</p>
+     <p>
+       <button onClick = {handelVote}>vote</button> 
+       <button onClick ={numberGenerator}>next anectode</button>
+     </p> 
+     <h1>Anectode with most votes</h1>
+     {anecdotes[mostVoted()]}
+     <p>Has {checkMax()} votes</p>
     </div>
   )
 }
